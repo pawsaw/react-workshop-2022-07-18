4b5067935e95181818752c3e346d96bcd1e6b725
Pawel Sawicki <public@psawicki.de>
2022-07-19 14:00:46 +0200

    BookList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import { useState } from 'react';
 import './App.css';
-import { Counter, OnValueChanged } from './components/Counter';
-import { SimpleName } from './components/SimpleName';
-import { Tabs, Tab } from './components/Tabs';
-
-
-
+import { BookList } from './components/BookList';
+import { books } from './data/books';
 
 function App() {
-
-  
-
   return (
     <div className="App">
-      <Tabs>
-        <Tab label="Foo">
-          Here comes foo
-        </Tab>
-        <Tab label="Bar">
-          Here comes bar
-        </Tab>
-      </Tabs>
+      <BookList books={books} onBookSelected={(book) => alert(book.price)} />
     </div>
   );
 }

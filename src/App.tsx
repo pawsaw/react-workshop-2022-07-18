132f4aaa8868d117633ee5e7244b95c38e24d985
Pawel Sawicki <public@psawicki.de>
2022-07-19 14:36:57 +0200

    BookDetail component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import { BookList } from './components/BookList';
-import { books } from './data/books';
+import { BookDetail } from './components/BookDetail';
+import { Book } from './domain/books';
 
 function App() {
+  const [book, setBook] = useState<Book | null>(null);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('http://localhost:4730/books/9781787125421');
+      const _book = await response.json();
+      setBook(_book);
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <BookList books={books} onBookSelected={(book) => alert(book.price)} />
+      {book ? <BookDetail book={book} /> : <span>Loading book...</span>}
+      {/* <BookList books={books} onBookSelected={(book) => alert(book.price)} /> */}
     </div>
   );
 }
---
+++ src/components/BookDetail/BookDetail.tsx
@@ -0,0 +1,14 @@
+import { Book } from '../../domain/books';
+
+export interface BookDetailProps {
+  book: Book;
+}
+
+export const BookDetail: React.FC<BookDetailProps> = ({ book }) => {
+  return (
+    <div>
+      <h3>{book.title}</h3>
+      <p>{book.abstract}</p>
+    </div>
+  );
+};
